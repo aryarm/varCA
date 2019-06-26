9429955758023d91bd9eb0bd15ea070a44b0f651
Arya Massarat <aryam7@gmail.com>
2019-06-26 15:30:21 -0700

    test, revise, and comment on prc code

--- scripts/prc.py
+++ scripts/prc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-# import gzip
+import math
 import pandas
 import argparse
 import numpy as np
@@ -15,59 +15,59 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+
+# read the file into a pandas data frame
 df = pd.read_csv(
-    args.table, sep=',', header=None, names=['caller', 'truth']
-    index_col=False, dtype=str, low_memory=False, na_values='.'
+    args.table, sep=',', header=None, names=['caller', 'truth'],
+    index_col=False, dtype=str, low_memory=False
 )
 
 
-categories = df['caller'].unique()
-rates = {
-    category: {
-        'TP': [],
-        'FP': [],
-        'TN': [],
-        'FN': []
-    } for category in categories
-}
+# extract the possible classes
+# (ex: INS, DEL, NA for a dataset of indels)
+classes = df['caller'].unique()
+# what are the test statistics?
+# true positives (TP), false positives (FP), false negatives (FN), and true
+# negatives (TN)
+statistics = ['TP', 'FP', 'FN', 'TN']
 
+# initialize a data structure for storing the test statistics at each threshold
+rates = np.zeros(
+    (len(classes), len(df.index), len(statistics)),
+    dtype='uint32'
+)
 
 thresh = 0
 curr_class = None
-for line in df:
-    if curr_class is not None and curr_class != line['truth']:
+for idx, line in df.iterrows():
+    if curr_class is not None and curr_class != tuple(line):
         # we've hit a threshold
-        # output rates
         thresh += 1
-    for category in categories:
-        if category == line['truth'] and category == line['caller']:
-            ctype = 'TP'
-        elif category == line['truth']:
-            ctype = 'FP'
-        elif category == line['caller']:
-            ctype = 'FN'
-        else:
-            ctype = 'TN'
-        # check if a new treshold number exists before adding to it
-        if thresh == rates[category][ctype].size:
-            rates[category][ctype].append(0)
-        # add to the current ctype number
-        rates[category][ctype][thresh] += 1
-    curr_class = line['truth']
+        # make sure to copy the statistics from the previous row for each class
+        for category in range(len(classes)):
+            rates[category][thresh] = rates[category][thresh-1]
+    for category in range(len(classes)):
+        # use binary to decimal conversion to figure out which statistic to +1
+        statistic = int(classes[category] == line['truth']) * 2 + \
+            int(classes[category] == line['caller'])
+        # add to the current statistic number
+        rates[category][thresh][statistic] += 1
+    curr_class = tuple(line)
 
-totals = {
-    category: {
-        ctype: rates[category][ctype][-1]
-        for ctype in rates[category]
-    } for category in categories
-}
+# clear some memory
+del df
+# make sure to delete unused np rows now that we know the number of thresholds
+# hopefully python garbage collection will free this memory?
+rates = rates[:, :thresh+1, :]
+
+# do other stuff...
 
 # calculate precision recall by micro-averaging for each class (at each threshold)
 for i in range(thresh+1):
     TP_sum = 0
     FP_sum = 0
     FN_sum = 0
-    for category in categories:
+    for category in classes:
         TP_sum += totals[category]['TP'][thresh]
         FP_sum += totals[category]['FP'][thresh]
         FN_sum += totals[category]['FN'][thresh]

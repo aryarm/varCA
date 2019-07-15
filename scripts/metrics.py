be9a9d9002c2920a447ae7eb2b8f3308ac3d54c8
Arya Massarat <aryam7@gmail.com>
2019-07-15 09:43:24 -0700

    allow metrics.py to calculate a positive and negative count

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -13,8 +13,15 @@ parser.add_argument(
     default=sys.stdout, help="the filename to save the data to"
 )
 parser.add_argument(
-    "-m", "--metrics", default='r,p,f', help="a comma separated list of metrics to output; use 'p' for precision, 'r' for recall, and 'f' for the F-beta score"
+    "-m", "--metrics", default='r,p,b,t,f', help=(
+        "a comma separated, ordered list of metrics to output; use 'p' for "
+        "precision, 'r' for recall, 'b' for the F-beta score, 't' for total "
+        "positives, and 'f' for total negatives"
+    )
 )
+# parser.add_argument(
+#     "-n", "--names", action='store_true', help="whether to include the names of the metrics alongside them"
+# )
 parser.add_argument(
     "table", nargs="?", default=sys.stdin,
     help="a two column (truth/predicted) table of variant classifications w/o a header"
@@ -24,16 +31,18 @@ args = parser.parse_args()
 # read the file into a pandas data frame
 df = pd.read_csv(
     args.table, sep='\t', header=None, names=['truth', 'predict'],
-    index_col=False, dtype={'probs': np.bool_, 'truth': np.bool_},
+    index_col=False, dtype={'predict': np.bool_, 'truth': np.bool_},
     low_memory=False, na_values='.'
 )
 
-to_idx = {'p': 0, 'r': 1, 'f': 2}
+to_idx = {'p': 0, 'r': 1, 'b': 2, 'f': 4, 't': 5}
 metrics = [to_idx[metric] for metric in args.metrics.split(",")]
-scores = np.array(
+scores = np.append(
     sklearn.metrics.precision_recall_fscore_support(
         df['truth'], df['predict'], average='binary'
-    )
+    ), sklearn.metrics.confusion_matrix(
+        df['truth'], df['predict']
+    ).sum(0)
 )
 
-np.savetxt(args.out, scores[metrics])
+np.savetxt(args.out, scores[metrics], fmt='%f')

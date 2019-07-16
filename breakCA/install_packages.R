177cdec68471cb76cca976652b24e622393405c4
Arya Massarat <aryam7@gmail.com>
2019-07-16 12:13:29 -0700

    remove unnecessary breakca scripts

--- breakCA/install_packages.R
+++
@@ -1,4 +0,0 @@
-install.packages(c('data.table', 'plyr', 'dplyr', 'pbapply', 'readr', 'reshape', 'rmutil', 'lattice', 'stringr', 'mlr', 'randomForest'), repos='https://cloud.r-project.org/')
-
-source("https://bioconductor.org/biocLite.R");
-biocLite(c("biovizBase", "rtracklayer", "Rsamtools", "BSgenome.Hsapiens.UCSC.hg19", "GenomicAlignments", "VariantAnnotation"))

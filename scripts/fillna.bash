1a558546f532919d8d1142e367e9589fe0ef6fb4
Arya Massarat <aryam7@gmail.com>
2019-08-16 17:41:11 -0700

    implement new fillna script for the new repo

--- scripts/fillna.bash
+++ scripts/fillna.bash
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # this script replaces NA values in a large TSV with the provided replacements
 
 # param1: the .tsv.gz file containing the large table for which we want to fill NA values
-# param2+: a regex pattern for a column followed by the NA value replacement for those cols (separated by a space)
+# param2+: the name of a column followed by the NA value replacement for that col (as two adjacent arguments)
 # 		   you can specify multiple column-value pairs
-# return (to stdout): the same table except with only the columns specified, where all NA values have been transformed
+# return (to stdout): the same table except where all specified NA values have been replaced
 
 
 script_dir="$(dirname "$BASH_SOURCE")";
@@ -15,7 +15,9 @@ for (( i=2; i<$#; i+=2 )); do
 	j=$((i+1))
 	reg="${!i}"
 	val="${!j}"
-	pfiles+=("<(zcat \"\$1\" | \"\$script_dir\"/cgrep.bash - -E '$reg' | awk -F '\t' -v 'OFS=\t' '{{for (i=1; i<=NF; i++) if (\$i==\".\") \$i=\"$val\"}}1')")
+	# retrieve the idx of the column among the columns
+	reg="$(zcat "$1" | head -n1 | tr '\t' '\n' | grep -Fn "$reg" | cut -f1 -d: | head -n1)"
+	pfiles+=("{if (\$$reg==\".\") \$$reg=\"$val\"}")
 done
 
-eval "paste ${pfiles[@]}"
+zcat "$1" | awk -F $'\t' -v 'OFS=\t' "${pfiles[*]} 1"

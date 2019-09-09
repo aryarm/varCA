c70223264a64ff198cc195dac7775267d12bc533
Arya Massarat <aryam7@gmail.com>
2019-09-08 22:47:32 -0700

    enable hyperparam tuning in training step

--- scripts/train_RF.R
+++ scripts/train_RF.R
@@ -20,8 +20,7 @@ importance<- args[5] # if specified, importance for each of the variables is sav
 library(plyr)
 library(dplyr)
 library(mlr)
-library(parallelMap)
-library(parallel)
+# library(tuneRanger)
 
 # load data.frame
 print("loading training data into R")
@@ -55,42 +54,27 @@ if (as.integer(balance)) {
 	rf.lrn$par.vals <- list(importance='impurity', verbose=TRUE)
 }
 
-# # create params
-# print("creating params to tune")
-# # mtry default: sqrt(number of features)
-# # nodesize default: 5
-# # ntree default: 500
-# params <- makeParamSet(makeIntegerParam("mtry",lower = 4,upper = 10),
-#                        makeIntegerParam("nodesize",lower = 10,upper = 20),
-#                        makeIntegerParam("ntree", lower = 50, upper=75))
-
-# #set validation strategy; 5-fold cross validation
-# rdesc <- makeResampleDesc("CV",iters=3L)
-
-# #set optimization technique
-# ctrl <- makeTuneControlRandom(maxit = 3L)
-
-# #create cluster
-# parallelStartSocket(20)
-# print("creating parallel sockets")
-
-# #start tuning for accuracy
-# tune <- tuneParams(learner = rf.lrn, task = traintask, resampling = rdesc,
-#                    measures = list(acc), par.set = params, control = ctrl,
-#                    show.info = T)
-# # tuned params
-# tune$x
-# # stop parallel
-# parallelStop()
-# print("tuned params for randomForest have been obtained")
-
-# # make model
-# rf.lrn$par.vals<- c(rf.lrn$par.vals, tune$x)
+if (as.integer(tune)) {
+	# mtry default: sqrt(number of features)
+	# nodesize default: 1
+	params <- makeParamSet(makeIntegerParam("mtry",lower = 4,upper = 12),
+                           makeIntegerParam("min.node.size",lower = 1,upper = 12))
+	# set validation strategy; 4-fold cross validation
+	rdesc <- makeResampleDesc("CV",iters=4L)
+	# set optimization technique
+	ctrl <- makeTuneControlGrid(resolution=8L)
+	# tune hyperparameters
+	print("tuning hyperparameters")
+	fit = tuneParams(learner=rf.lrn, task=traintask, resampling=rdesc, measures=list(acc), par.set=params, control=ctrl, show.info=T)
+	print("tuned params are")
+	tune$x
+	rf.lrn$par.vals = c(rf.lrn$par.vals, tune$x)
+}
 print("training model")
-fit= mlr::train(rf.lrn, traintask)
+fit = mlr::train(rf.lrn, traintask)
 
 # print out variable importance
-if (importance != "") {
+if (!is.na(importance)) {
 	print("recording variable importance:")
 	importance_df = as.data.frame(sort(fit$learner.model$variable.importance, decreasing=TRUE))
 	print(importance_df)

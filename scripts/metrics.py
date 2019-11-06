a789f32633332e8ad7dcb7d63dfdcc48366e3c5c
Arya Massarat <aryam7@gmail.com>
2019-11-05 23:54:44 -0800

    add auroc and avg precision to single pt calculations

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -13,37 +13,62 @@ parser.add_argument(
     default=sys.stdout, help="the filename to save the data to"
 )
 parser.add_argument(
-    "-m", "--metrics", default='r,p,b,t,f', help=(
+    "-m", "--metrics", default='r,p,b,t,f,a,v', help=(
         "a comma separated, ordered list of metrics to output; use 'p' for "
         "precision, 'r' for recall, 'b' for the F-beta score, 't' for total "
-        "positives, and 'f' for total negatives"
+        "positives, 'f' for total negatives, 'a' for the AUROC, and 'v' for "
+        "the avg precision score"
     )
 )
 # parser.add_argument(
 #     "-n", "--names", action='store_true', help="whether to include the names of the metrics alongside them"
 # )
 parser.add_argument(
+    "-p", "--ignore-probs", action='store_true', help="whether to only read truth and predict columns and ignore a probs column if it is provided; note that the AUROC and avg precision will not be output"
+)
+parser.add_argument(
     "table", nargs="?", default=sys.stdin,
-    help="a two column (truth/predicted) table of variant classifications w/o a header"
+    help="a three column (truth/predicted/probs) table of variant classifications w/o a header"
 )
 args = parser.parse_args()
 
+# which cols should we read?
+fields = ['truth', 'predict', 'probs']
+dtypes = {'predict': np.bool_, 'truth': np.bool_, 'probs': np.float_}
+if args.ignore_probs:
+    fields = fields[:2]
+    dtypes.pop('probs')
 # read the file into a pandas data frame
 df = pd.read_csv(
-    args.table, sep='\t', header=None, names=['truth', 'predict'],
-    index_col=False, dtype={'predict': np.bool_, 'truth': np.bool_},
+    args.table, sep='\t', header=None, names=fields,
+    index_col=False, dtype=dtypes,
     low_memory=False, na_values='.'
 )
 df.fillna(0, inplace=True)
 
-to_idx = {'p': 0, 'r': 1, 'b': 2, 'f': 4, 't': 5}
-metrics = [to_idx[metric] for metric in args.metrics.split(",")]
+# calculate the metrics
 scores = np.append(
     sklearn.metrics.precision_recall_fscore_support(
-        df['truth'], df['predict'], average='binary'
-    ), sklearn.metrics.confusion_matrix(
-        df['truth'], df['predict']
-    ).sum(0)
+        df['truth'], df['predict'], beta=0.5, average='binary'
+    ),
+    sklearn.metrics.confusion_matrix(df['truth'], df['predict']).sum(0)
 )
+# calculate additional metrics if we can
+if not args.ignore_probs:
+    scores = np.append(
+        scores,
+        np.array([
+            sklearn.metrics.roc_auc_score(df['truth'], df['probs']),
+            sklearn.metrics.average_precision_score(df['truth'], df['probs'])
+        ])
+    )
+
+# which metrics should we return?
+to_idx = {'p': 0, 'r': 1, 'b': 2, 'f': 4, 't': 5, 'a': 6, 'v': 7}
+metrics = [
+    to_idx[metric]
+    for metric in args.metrics.split(",")
+    if not args.ignore_probs or metric not in {'a', 'v'}
+]
 
 np.savetxt(args.out, scores[metrics], fmt='%f')

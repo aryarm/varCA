d43237859c196f0847567c09696e92603bde32d7
Arya Massarat <aryam7@gmail.com>
2019-12-06 21:37:55 -0800

    create sample data and pipeline

--- run.bash
+++ run.bash
@@ -33,25 +33,25 @@ fi
 # fastq files for each of your samples.
 
 # prepare pipeline -- extract features for each site to prepare for classifying
-snakemake \
--s Snakefiles/Snakefile-prepare \
---cluster "qsub -t 1 -V -q iblm.q -j y -o ${out_path}/qlog" \
--j 24 \
---config output_dir="${out_path}" \
---latency-wait 60 \
---use-conda \
--k \
-"$@" &>"${out_path}/log"
+#snakemake \
+#-s Snakefiles/Snakefile-prepare \
+#--cluster "qsub -t 1 -V -q iblm.q -j y -o ${out_path}/qlog" \
+#-j 24 \
+#--config output_dir="${out_path}" \
+#--latency-wait 60 \
+#--use-conda \
+#-k \
+#"$@" &>"${out_path}/log"
 
 # split the output of the prepare pipeline into a training and testing set
 # (only if the output of the prepare pipeline has changed since last time)
-{
-	final="$out_path"/merged_snp/SRR891269
-	([ ! -f "$final"/odd-chrom.tsv.gz ] || [ -n "$(find -L "$final"/final.tsv.gz -prune -newer "$final"/odd-chrom.tsv.gz -exec echo . \;)" ]) && \
-	zcat "$final"/final.tsv.gz | { read -r head && echo "$head" && awk -F'\t' -v 'OFS=\t' '$1 ~ /^[0-9]+$/ && $1%2'; } | gzip > "$final"/odd-chrom.tsv.gz
-	([ ! -f "$final"/even-chrom.tsv.gz ] || [ -n "$(find -L "$final"/final.tsv.gz -prune -newer "$final"/even-chrom.tsv.gz -exec echo . \;)" ]) && \
-	zcat "$out_path"/merged_snp/SRR891269/final.tsv.gz | { read -r head && echo "$head" && awk -F'\t' -v 'OFS=\t' '$1 ~ /^[0-9]+$/ && $1%2 == 0'; } | gzip > "$final"/even-chrom.tsv.gz
-} >>"${out_path}/log" 2>&1
+#{
+#	final="$out_path"/merged_snp/SRR891269
+#	([ ! -f "$final"/odd-chrom.tsv.gz ] || [ -n "$(find -L "$final"/final.tsv.gz -prune -newer "$final"/odd-chrom.tsv.gz -exec echo . \;)" ]) && \
+#	zcat "$final"/final.tsv.gz | { read -r head && echo "$head" && awk -F'\t' -v 'OFS=\t' '$1 ~ /^[0-9]+$/ && $1%2'; } | gzip > "$final"/odd-chrom.tsv.gz
+#	([ ! -f "$final"/even-chrom.tsv.gz ] || [ -n "$(find -L "$final"/final.tsv.gz -prune -newer "$final"/even-chrom.tsv.gz -exec echo . \;)" ]) && \
+#	zcat "$out_path"/merged_snp/SRR891269/final.tsv.gz | { read -r head && echo "$head" && awk -F'\t' -v 'OFS=\t' '$1 ~ /^[0-9]+$/ && $1%2 == 0'; } | gzip > "$final"/even-chrom.tsv.gz
+#} >>"${out_path}/log" 2>&1
 # the above code is only included here so that this file can be executed on the example data all at once
 # rather than executing this code, you should manually execute whichever commands will help you prepare the data for the classify pipeline
 # (if you're not training the classifier, you probably won't even need to execute any intermediate commands at all)
@@ -65,6 +65,5 @@ snakemake \
 --latency-wait 60 \
 --use-conda \
 -k \
---notemp \
 "$@" >>"${out_path}/log" 2>&1
 

9704ce2711dfeecf7aa58de9fd7072ad89c3f78d
Arya Massarat <23412689+aryam7@users.noreply.github.com>
2020-06-16 07:35:36 -0700

    fix vcf header coming from 2vcf.py (see issue #6)

--- scripts/2vcf.py
+++ scripts/2vcf.py
@@ -11,9 +11,6 @@ from sklearn.metrics import accuracy_score, recall_score
 from sklearn.metrics import roc_curve
 
 
-# strategy: iterate through each line in the results file and seek to the corresponding line in the merged file
-#results = pd.read_csv(args.results, sep="\t", low_memory=True, dtype="str", na_values=['.'], chunksize=100)
-
 
 # def plot_precision_probs(a):
 #   a = np.loadtxt(a)[1:,1:]
@@ -50,9 +47,9 @@ def plot_accuracy_probs(results, bin_size):
 def phred(val):
     return -10*np.log10(1-val)
 
-def plot_tpr_probs(df, bin_size):
+def plot_tpr_probs(results, bin_size):
     """ bin the sites and calculate an accuracy for that bin """
-    df = df.sort_values(by='breakca~prob.1')
+    df = pd.read_csv(results, sep="\t", header=0, index_col=["CHROM", "POS"], usecols=['CHROM', 'POS', 'breakca~truth', 'breakca~prob.1', 'breakca~CLASS:']).sort_values(by='breakca~prob.1')
     # split the dataset into high (above 0.5) vs low (below 0.5) scores
     df1 = df[df['breakca~truth']>=0.5]
     df2 = df[df['breakca~truth']<0.5][::-1]
@@ -61,18 +58,38 @@ def plot_tpr_probs(df, bin_size):
         for grp in (df1.iloc[i:i+bin_size] for i in range(0,len(df1)-bin_size+1,bin_size))
     ])
     bin_size = int(bin_size*len(df2)/len(df1))
-    lst2 = np.array([
-        (grp.iloc[:,1].mean(), phred(recall_score(grp.iloc[:,0], grp.iloc[:,2], pos_label=0)))
-        for grp in (df2.iloc[i:i+bin_size] for i in range(0,len(df2)-bin_size+1,bin_size))
-    ])
-    lst3 = np.array([
-        (grp.iloc[:,1].mean(), phred(recall_score(grp.iloc[:,0], grp.iloc[:,2])))
-        for grp in (df.iloc[i:i+bin_size] for i in range(0,len(df)-bin_size+1,bin_size))
-    ])
+    # lst2 = np.array([
+    #     (grp.iloc[:,1].mean(), phred(recall_score(grp.iloc[:,0], grp.iloc[:,2], pos_label=0)))
+    #     for grp in (df2.iloc[i:i+bin_size] for i in range(0,len(df2)-bin_size+1,bin_size))
+    # ])
+    # lst3 = np.array([
+    #     (grp.iloc[:,1].mean(), phred(recall_score(grp.iloc[:,0], grp.iloc[:,2])))
+    #     for grp in (df.iloc[i:i+bin_size] for i in range(0,len(df)-bin_size+1,bin_size))
+    # ])
     #plt.plot(*lst.T, '--bo')
     #plt.xlabel("Predicted Probability")
     #plt.ylabel("QUAL (Phred-Scaled Recall)")
     return lst, lst2, lst3
+
+# def plot_tpr_probs(results, bin_size):
+#     """ bin the sites and calculate an accuracy for that bin """
+#     df = pd.read_csv(results, sep="\t", header=0, index_col=["CHROM", "POS"], usecols=['CHROM', 'POS', 'breakca~truth', 'breakca~prob.1', 'breakca~CLASS:']).sort_values(by='breakca~prob.1')
+#     # split the dataset into high (above 0.5) vs low (below 0.5) scores
+#     df1 = df[df['breakca~truth']>=0.5]
+#     df2 = df[df['breakca~truth']<0.5][::-1]
+#     lst = np.array([
+#         (grp[1].iloc[:,1].mean(), recall_score(grp[1].iloc[:,0], grp[1].iloc[:,2]), len(grp[1]))
+#         for grp in df1.groupby(pd.cut(df1['breakca~prob.1'], pd.interval_range(0.5, 1, bin_size)))
+#     ])
+#     bin_size = int(bin_size*len(df2)/len(df1))
+#     # lst2 = np.array([
+#     #     (grp[1].iloc[:,1].mean(), recall_score(grp[1].iloc[:,0], grp[1].iloc[:,2], pos_label=0))
+#     #     for grp in df1.groupby(pd.cut(df1['breakca~prob.1'], pd.interval_range(0, 0.5, bin_size)))
+#     # ])
+#     #plt.plot(*lst.T, '--bo')
+#     #plt.xlabel("Predicted Probability")
+#     #plt.ylabel("QUAL (Phred-Scaled Recall)")
+#     return lst
 
 # def prob_phred(results):
 #     df = pd.read_csv(results, sep="\t", header=0, index_col=["CHROM", "POS"], usecols=['CHROM', 'POS', 'breakca~truth', 'breakca~prob.1', 'breakca~CLASS:']).sort_values(by='breakca~prob.1')
@@ -81,21 +98,26 @@ def plot_tpr_probs(df, bin_size):
 #     plt.plot(range(len(prob1['phred'])), prob1['phred'])
 
 
+
+
 def strip_type(caller):
     """
         strip the -indel or -snp from the end of a caller name
     """
     if caller.endswith('-snp'):
-        return caller[:-len('-snp')]
+        caller = caller[:-len('-snp')]
     elif caller.endswith('-indel'):
-        return caller[:-len('-indel')]
-    else:
-        return caller
+        caller = caller[:-len('-indel')]
+    # if there is still a dash, get everything after it
+    i = caller.rfind('-')
+    if i != -1:
+        caller = caller[i+1:]
+    return caller
 
 def isnan(val):
     return type(val) is float and np.isnan(val)
 
-def get_calls(prepared, callers=None):
+def get_calls(prepared, callers=None, pretty=False):
     """
         get the alleles in each row of prepared at the location (CHROM/POS) of loc
         when choosing an alt allele, choose from the callers in the order given
@@ -113,12 +135,12 @@ def get_calls(prepared, callers=None):
                 if caller.endswith('~ALT') and not caller.startswith('pg-')
             ]
         if callers is not dict:
-            callers = {caller:strip_type(caller) for caller in callers}
+            callers = {caller: (strip_type(caller) if pretty else caller) for caller in callers}
         # now iterate through each row (and also convert the POS column to an int)
         for idx, row in chunk.iterrows():
             # check if we already passed the row -- ie we either:
-            # 1) we moved onto a new contig or
-            # 2) we moved passed the position
+            # 1) moved onto a new contig or
+            # 2) moved passed the position
             while (
                 idx[0] != loc[0] and loc[0] in contigs
             ) or (
@@ -128,6 +150,7 @@ def get_calls(prepared, callers=None):
                 loc, predict = yield None
             if idx == loc:
                 # we found it!
+                found = False
                 # now, we must figure out which caller to get the alleles from
                 for caller in callers:
                     ref, alt = row[caller+"~REF"], row[caller+"~ALT"]
@@ -136,12 +159,23 @@ def get_calls(prepared, callers=None):
                     if not isnan(ref) and (
                         (isnan(alt) + predict) % 2
                     ):
+                        found = True
                         break
-                loc, predict = yield callers[caller], ref, alt
+                if found:
+                    loc, predict = yield callers[caller], ref, alt
+                else:
+                    # if we know there is a variant here, but none of the other
+                    # callers found it, just label it as a non-variant
+                    # TODO: figure out the alt allele from inspecting the ref genome?
+                    loc, predict = yield 'varca', row['REF'], float('nan')
             # save the current contig so that we know which ones we've seen
             contigs.add(idx[0])
 
-def main(prepared, classified, callers=None, cs=1000, all_sites=False):
+def prob2qual(prob):
+    # TODO: scale qual by trained linear regression model
+    return -10*np.log10(1-prob)
+
+def main(prepared, classified, callers=None, cs=1000, all_sites=False, pretty=False):
     """
         use the results of the prepare pipeline and the classify pipeline
         to create a VCF with all of the classified sites
@@ -151,15 +185,14 @@ def main(prepared, classified, callers=None, cs=1000, all_sites=False):
         pd.read_csv(
             prepared, sep="\t", header=0, index_col=["CHROM", "POS"],
             dtype=str, chunksize=cs, na_values="."
-        ), callers
+        ), callers, pretty
     )
     # flush out the first item in the generator
     next(prepared)
     # also retrieve the classifications as a df
     classified = pd.read_csv(
         classified, sep="\t", header=0, index_col=["CHROM", "POS"],
-        dtype={'CHROM':str, 'POS':int}, chunksize=cs, na_values=".",
-        usecols=['CHROM', 'POS', 'breakca~truth', 'breakca~prob.1', 'breakca~CLASS:']
+        dtype={'CHROM':str, 'POS':int}, chunksize=cs, na_values="."
     )
     # keep track of how many sites in the classifications df we've had to skip
     skipped = 0
@@ -178,8 +211,7 @@ def main(prepared, classified, callers=None, cs=1000, all_sites=False):
                 skipped += 1
                 continue
             caller, ref, alt = call
-            # TODO: scale qual by trained linear regression model
-            qual = -10*np.log10(1-row['breakca~prob.1'])
+            qual = prob2qual(row['breakca~prob.'+str(int(not isnan(alt)))])
             # construct a dictionary with all of the relevant details
             yield {
                 'contig': str(idx[0]), 'start': idx[1], 'stop': idx[1]+len(ref),
@@ -196,7 +228,7 @@ def write_vcf(out, records):
     """
     vcf = pysam.VariantFile(args.out, mode='w')
     # write the necessary VCF header info
-    vcf.header.info.add("CALLER", 1, 'String', "The caller from which this site was taken", Source='varca')
+    vcf.header.info.add("CALLER", 1, 'String', "The caller from which this site was taken")
     contigs = set()
     for rec in records:
         # handle pysam increasing the start and end sites by 1
@@ -220,6 +252,7 @@ def write_vcf(out, records):
             )
         # write the record to a file
         vcf.write(record)
+    return vcf
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -230,17 +263,24 @@ if __name__ == "__main__":
         "classified", type=Path, help="a sorted, results.tsv.gz file from the output of the classify pipeline"
     )
     parser.add_argument(
-        "prepared", type=Path, nargs="?", default=sys.stdin, help="a sorted, merge.tsv.gz file from the prepare pipeline"
+        "prepared", type=Path, nargs="?", default=sys.stdin, help="a sorted, merge.tsv.gz file from the prepare pipeline (if not supplied, this is read from stdin)"
     )
     parser.add_argument(
         '-c', "--callers", default=None, help="a comma separated list of the callers from which to choose alleles, supplied in order of priority (default: all of the callers in the file, in the order they appear)"
     )
     parser.add_argument(
-        '-s', "--chunksize", type=np.uint32, default=1000, help="how many rows to read into memory at once"
+        '-s', "--chunksize", type=np.uint32, default=100000, help="how many rows to read into memory at once (default: 100,000)"
     )
     parser.add_argument(
         '-a', '--all', action='store_true', help="whether to also write non-variant sites to create a gVCF (default: no)"
     )
+    parser.add_argument(
+        '-p', '--pretty', action='store_true', help="should caller names appear in the vcf by their pretty form (with all dashes intelligently removed) or their original caller ID form? (default: the original form)"
+    )
+    parser.add_argument(
+        '-n', '--nothing', action='store_true', help="do absolutely nothing, except read the arguments (for testing and internal use)"
+    )
     args = parser.parse_args()
 
-    write_vcf(args.out, main(args.prepared, args.classified, args.callers, args.chunksize, args.all))
+    if not args.nothing:
+        vcf = write_vcf(args.out, main(args.prepared, args.classified, args.callers.split(","), args.chunksize, args.all, args.pretty))
